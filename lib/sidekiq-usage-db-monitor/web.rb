07b92e31bbc6772d40799742d45166606c6db34c
Eric Abrahamsn <ericthredsbeard@gmail.com>
2017-12-17 22:51:44 -0500

    Add web file for sidekiq tab.

---
+++ lib/sidekiq-usage-db-monitor/web.rb
@@ -0,0 +1,12 @@
+require "sidekiq/sidekiq-usage-db-monitor/web_extension"
+
+if defined?(Sidekiq::Web)
+  Sidekiq::Web.register Sidekiq::UsageDbMonitor::WebExtension
+
+  if Sidekiq::Web.tabs.is_a?(Array)
+    # For sidekiq < 2.5
+    Sidekiq::Web.tabs << "Usage DB Monitor"
+  else
+    Sidekiq::Web.tabs["Usage DB Monitor"] = "Usage DB Monitor"
+  end
+end
