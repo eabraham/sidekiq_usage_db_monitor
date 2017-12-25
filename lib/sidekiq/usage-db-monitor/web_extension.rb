a97cea52403cfdf4bf04cf6df5325002a147d239
Eric Abrahamsn <ericthredsbeard@gmail.com>
2017-12-25 16:44:15 -0600

    Add timescale graph for table duration.

--- lib/sidekiq/usage-db-monitor/web_extension.rb
+++ lib/sidekiq/usage-db-monitor/web_extension.rb
@@ -3,9 +3,31 @@ module Sidekiq
     module WebExtension
       def self.registered(app)
         app.get '/usage-db-monitor' do
-	  view_path    = File.join(File.expand_path("..", __FILE__), "views")
+	        view_path  = File.join(File.expand_path("..", __FILE__), "views")
+          asset_path = File.join(File.expand_path("..", __FILE__), "assets")
 
-          render(:erb, File.read(File.join(view_path, "usage_db_monitor.erb")))
+          render(:erb, File.read(File.join(view_path, "usage_db_monitor.erb")), locals: {view_path: view_path, asset_path: asset_path})
+        end
+
+        app.get '/table-duration' do
+          min_since_epoch = params['min_since_epoch'].to_i
+
+          action = 'read'
+          @redis = Redis.new({})
+          durations = @redis.hgetall("sidekiq-job-planner-by-table-duration-#{action}-#{min_since_epoch}")
+
+          tables = ActiveRecord::Base.connection.tables
+
+          tables.each do |table, value|
+            durations[table] = durations[table].to_i
+          end
+          payload = {
+              durations: durations,
+              metadata:  {
+                min_since_epoch: min_since_epoch
+              }
+            }
+          json(payload)
         end
       end
     end
